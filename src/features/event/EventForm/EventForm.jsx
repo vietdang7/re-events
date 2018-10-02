1e6dc7c2fdee0127020f9609e7e584b8b7fbec2a
Viet Dang <thedangs7@gmail.com>
2018-10-02 21:38:02 +0300

    add componentWillReceiveProps to EventForm

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -23,6 +23,15 @@ class EventForm extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+
+    if (nextProps.selectedEvent !== this.props.selectedEvent) {
+      this.setState({
+        event: nextProps.selectedEvent || emptyEvent
+      })
+    }
+  } 
+
   onFormSubmit = (evt) => {
     evt.preventDefault();
     this.props.createEvent(this.state.event);
