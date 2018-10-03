2624d8eb8e6b164e175c6511f712192992a13282
Viet Dang <thedangs7@gmail.com>
2018-10-03 12:10:57 +0300

    Add routing for components

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -3,6 +3,11 @@ import { Container } from 'semantic-ui-react';
 import { Route } from 'react-router-dom';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
 
 class App extends Component {
   render() {
@@ -10,7 +15,14 @@ class App extends Component {
       <div>
         <NavBar />
         <Container className="main">
+          <Route path='/' component={EventDashboard} />
           <Route path='/events' component={EventDashboard} />
+          <Route path='/event/:id' component={EventDetailedPage} />
+          <Route path='/people' component={PeopleDashboard} />
+          <Route path='/profile/:id' component={UserDetailedPage} />
+          <Route path='/settings' component={SettingsDashboard} />
+          <Route path='/createEvent' component={EventForm} />
+          
         </Container>
         
       </div>
