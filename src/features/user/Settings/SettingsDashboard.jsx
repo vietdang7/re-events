53b7c11892522a6446a00ff3a0c46bfa8f79fbea
Viet Dang <thedangs7@gmail.com>
2018-10-04 16:50:39 +0300

    Update SettingsDashboard and SettingsNav

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import SettingNav from './SettingsNav';
 
 const SettingsDashboard = () => {
   return (
     <Grid>
-      <Grid.Column >
+      <Grid.Column width={12} >
         <h1>Settings</h1>
+      </Grid.Column>
+      <Grid.Column width={4} >
+        <SettingNav />
       </Grid.Column>
     </Grid>
   )
--- src/features/user/Settings/SettingsNav.jsx
+++ src/features/user/Settings/SettingsNav.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Grid, Header, Menu } from 'semantic-ui-react'
 
 const SettingsNav = () => {
   return (
-    <div>
-      <h1>This is settings nav</h1>
-    </div>
-  )
-}
+    <Grid.Column width={4}>
+      <Menu vertical>
+        <Header icon="user" attached inverted color="grey" content="Profile" />
+        <Menu.Item>Basics</Menu.Item>
+        <Menu.Item>About Me</Menu.Item>
+        <Menu.Item>My Photos</Menu.Item>
+      </Menu>
+      <Grid.Row />
+      <Menu vertical>
+        <Header
+          icon="settings"
+          attached
+          inverted
+          color="grey"
+          content="Account"
+        />
+        <Menu.Item>My Account</Menu.Item>
+      </Menu>
+    </Grid.Column>
+  );
+};
 
-export default SettingsNav
+export default SettingsNav;
