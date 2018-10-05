2a0db7d8ec79f554af1db6ccbac49031cc3ecdba
Viet Dang <thedangs7@gmail.com>
2018-10-05 23:42:03 +0300

    import needed components for Settingsdashboard

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Switch, Route } from 'semantic-ui-react'
-import SettingNav from './SettingsNav';
+import { Switch, Route } from 'react-router-dom';
+import SettingsNav from './SettingsNav';
+import BasicsPage from './BasicsPage';
+import AboutPage from './AboutPage';
+import PhotosPage from './PhotosPage';
+import AccountPage from './AccountPage';
+
 
 const SettingsDashboard = () => {
   return (
     <Grid>
       <Grid.Column width={12} >
         <Switch>
-          <Route  />
+          <Route path='/settings/basics' component={BasicsPage}/>
+          <Route path='/settings/about' component={AboutPage}/>
+          <Route path='/settings/photos' component={PhotosPage}/>
+          <Route path='/settings/account' component={AccountPage}/>
         </Switch>
       </Grid.Column>
       <Grid.Column width={4} >
-        <SettingNav />
+        <SettingsNav />
       </Grid.Column>
     </Grid>
   )
