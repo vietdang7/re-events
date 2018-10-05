c67a7e833bf7cf0f682b82f4e61a1e181f1051e2
Viet Dang <thedangs7@gmail.com>
2018-10-05 23:38:04 +0300

    Update SettingsDashboard

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import { Switch, Route } from 'semantic-ui-react'
 import SettingNav from './SettingsNav';
 
 const SettingsDashboard = () => {
   return (
     <Grid>
       <Grid.Column width={12} >
-        <h1>Settings</h1>
+        <Switch>
+          <Route  />
+        </Switch>
       </Grid.Column>
       <Grid.Column width={4} >
         <SettingNav />
