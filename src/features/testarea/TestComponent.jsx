012ba023a25a7f03e91bbb009c449df17f7ac1fd
Viet Dang <thedangs7@gmail.com>
2018-10-07 21:26:15 +0300

    update TestComponent.js

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class TestComponent extends Component {
   render() {
     return (
       <div>
         <h1>Test area</h1>
+        <h3> The answer is: {this.props.data} </h3>
       </div>
     )
   }
 }
 
-export default TestComponent
+const mapStateToProps = (state) => {
+  data: state.test.data
+}
+
+export default connect(mapStateToProps)(TestComponent);
