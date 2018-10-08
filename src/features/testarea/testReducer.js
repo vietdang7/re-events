1be7528134fe8feb6387476a9920a95bc42537d5
Viet Dang <thedangs7@gmail.com>
2018-10-08 20:42:43 +0300

    update testReducer.js

--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,9 +1,18 @@
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./testConstants";
+
 const initialState = {
   data: 48
 };
 
 const testReducer = (state = initialState, action) => {
-  return state
+  switch (action.type) {
+    case INCREMENT_COUNTER:
+      return { ...state, data: state.data + 1 };
+    case DECREMENT_COUNTER:
+      return {...state, data: state.data - 1};
+    default:
+      return state;
+  }
 }
 
 export default testReducer;
