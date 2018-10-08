0801dea4bdf308b0dd3ac4456991c2d112a57fb2
Viet Dang <thedangs7@gmail.com>
2018-10-08 19:59:30 +0300

    use composeWithDevTools in configureStore.js

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers/rootReducer';
 
 export const configureStore = (preloadedState) => {
   const middlewares = [];
   const middlewareEnhancer = applyMiddleware(...middlewares);
   const storeEnhancers = [middlewareEnhancer];
-  const composedEnhancer = compose(...storeEnhancers);
+  const composedEnhancer = composeWithDevTools(...storeEnhancers);
   const store = createStore(
     rootReducer,
     preloadedState,
