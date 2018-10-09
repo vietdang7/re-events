b2e2bc41cb35f13e2f2b4a667309a5b674b3aaf5
Viet Dang <thedangs7@gmail.com>
2018-10-09 14:22:40 +0300

    Update TestComponent.jsx

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Button } from 'semantic-ui-react';
+import { incrementCounter, decrementCounter } from './testActions';
 
 class TestComponent extends Component {
   render() {
+    const {incrementCounter, decrementCounter,data} = this.props;
     return (
       <div>
         <h1>Test area</h1>
-        <h3> The answer is: {this.props.data} </h3>
+        <h3> The answer is: {data} </h3>
+        <Button onClick={incrementCounter} content="+"/>
+        <Button onClick={decrementCounter} content="-"/>
       </div>
     )
   }
@@ -14,5 +19,9 @@ class TestComponent extends Component {
 
 const mapStateToProps = (state) => ({data: state.test.data})
 
+const mapDispatchToProps = {
+  incrementCounter,
+  decrementCounter
+}
 
-export default connect(mapStateToProps)(TestComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(TestComponent);
