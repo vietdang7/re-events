c5018ee6fccc655f54c49fd5019d4f58ebf701d1
Viet Dang <thedangs7@gmail.com>
2018-10-09 16:14:58 +0300

    update rootReducer.js

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -3,7 +3,8 @@ import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
 
 const rootReducer = combineReducers({
-  test: testReducer
+  test: testReducer,
+  events: eventReducer
 })
 
 export default rootReducer;
