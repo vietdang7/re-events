1e114e7185bfeec0415c985213ca185a1426e774
Viet Dang <thedangs7@gmail.com>
2018-10-11 15:34:51 +0300

    Update EventDetailedSidebar

---
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventDetailedSidebar = () => {
+  return (
+    <div>
+      <h1>Detailed Side bar</h1>
+    </div>
+  )
+}
+
+export default EventDetailedSidebar
