6f23f95d4a1411e1b208dfaa298ae913372deb36
Viet Dang <thedangs7@gmail.com>
2018-10-14 21:14:48 +0300

    update index.js with ScrollToTop.jsx

--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </BrowserRouter>
   </Provider>
   ,
