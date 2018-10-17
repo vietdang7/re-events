78d68840648889b793d0b2255f311989666a22be
Viet Dang <thedangs7@gmail.com>
2018-10-17 15:46:44 +0300

    update TextInput.jsx

--- src/app/common/form/TextInput.jsx
+++ src/app/common/form/TextInput.jsx
@@ -5,6 +5,7 @@ function TextInput({input, width, type, placeholder, meta: {touched, error}}) {
   return (
     <Form.Field error={touched && !!error} width={width} >
       <input {...input} placeholder={placeholder} type={type} />
+      {touched && error && <Label basic color='red'>{error}</Label>}
     </Form.Field>
   )
 }
