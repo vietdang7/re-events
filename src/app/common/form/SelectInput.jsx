a97b2c741d400bf3adcca7d7b370244f4c78b313
Viet Dang <thedangs7@gmail.com>
2018-10-21 17:32:53 +0300

    create SelectInput.jsx

---
+++ src/app/common/form/SelectInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Form, Label, Select } from 'semantic-ui-react';
+
+const SelectInput = ({input, type, placeholder, multiple, options, meta:{touched, error}}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SelectInput
