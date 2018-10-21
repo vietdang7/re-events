57e0d00f4ca2f0897f8e99ef1bc284ed6b65f694
Viet Dang <thedangs7@gmail.com>
2018-10-21 18:40:51 +0300

    update SelectInput.jsx

--- src/app/common/form/SelectInput.jsx
+++ src/app/common/form/SelectInput.jsx
@@ -3,9 +3,15 @@ import { Form, Label, Select } from 'semantic-ui-react';
 
 const SelectInput = ({input, type, placeholder, multiple, options, meta:{touched, error}}) => {
   return (
-    <div>
-      
-    </div>
+    <Form.Field error={touched && !!error}>
+      <Select
+        value={input.value || null}
+        onChange={(e, data) => input.onChange(data.value)}
+        placeholder={placeholder}
+        options={options}
+        multiple={multiple}
+      />
+    </Form.Field>
   )
 }
 
